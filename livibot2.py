549556607d5816ae481562439aeeb82bce84064d
iipizza-wq <iipizza@rambler.ru>
2026-08-27 11:46:16 +0300

    Update livibot2.py

--- livibot2.py
+++ livibot2.py
@@ -162,6 +162,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(
             COMPLEX_TEXT,
             reply_markup=CLUB_KEYBOARD,
+            parse_mode='Markdown'
         )
         return
 
